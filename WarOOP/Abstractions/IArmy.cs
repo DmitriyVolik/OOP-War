f85c29578c6c45bd48269c836a2644bc6660a735
DmitriyVolik <60630678+DmitriyVolik@users.noreply.github.com>
2022-02-20 10:05:41 +0200

    Fix

--- WarOOP/Abstractions/IArmy.cs
+++ WarOOP/Abstractions/IArmy.cs
@@ -4,7 +4,7 @@ namespace WarOOP.Abstractions;
 
 public interface IArmy
 {
-    public void AddUnits<T>(int count) where T : Warrior, new();
+    void AddUnits<T>(int count) where T : Warrior, new();
     
     public Warrior GetUnit();
 }
--- WarOOP/Program.cs
+++ WarOOP/Program.cs
@@ -1,13 +1,10 @@
 
-using System.Threading.Channels;
-using WarOOP.Models;
-
 namespace WarOOP;
 
 public static class Program
 {
     public static void Main()
     {
-        
+
     }
 }
